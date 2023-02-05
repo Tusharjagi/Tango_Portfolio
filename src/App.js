8300b4ecc9f539b53f08d0956d04c14a67ecd2c5
Tusharjagi <tusharjagi@gmail.com>
2023-02-05 18:29:31 +0530

    react setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Example from "./Components/Example";
+
+const App = () => {
+  return (
+    <>
+      <Example />
+      <h1>Hello React</h1>
+    </>
+  );
+};
+
+export default App;
